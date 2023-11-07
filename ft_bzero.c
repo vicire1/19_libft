070c6657654a073a7c4e2b60b1f63839e5b40768
Victor Decleire <vdecleir@fu-r2-p5.s19.be>
2023-11-07 10:58:00 +0100

    lasttry

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: vdecleir <vdecleir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 18:15:03 by victor            #+#    #+#             */
-/*   Updated: 2023/11/06 12:23:49 by vdecleir         ###   ########.fr       */
+/*   Updated: 2023/11/07 10:25:28 by vdecleir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,24 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	char	*str;
-	size_t	i;
+	unsigned char	*str;
 
-	i = 0;
-	str = (char *)s;
-	while (i < n)
+	str = (unsigned char *)s;
+	while (n)
 	{
-		str[i] = '\0';
-		i++;
+		str[n - 1] = '\0';
+		n--;
 	}
 }
-/*
-#include <stdio.h>
-#include <string.h>
 
-int	main(void)
-{
-	char s[] = "Victor";
-	char t[] = "Victor";
-	ft_bzero(s, 1);
-	bzero(t, 1);
-	printf("s0 : %c t0 : %c\ns2 : %c t2 : %c", s[0], t[0], s[1], t[1]);
-}*/
+// #include <stdio.h>
+// #include <string.h>
+
+// int	main(void)
+// {
+// 	char s[999999999999999] = "Victor";
+// 	char t[999999999999999] = "Victor";
+// 	ft_bzero(s, 99999999999999);
+// 	bzero(t, 99999999999999);
+// 	printf("s0 : %c t0 : %c\ns1 : %c t1 : %c", s[0], t[0], s[1], t[1]);
+// }
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vdecleir <vdecleir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 17:32:15 by vdecleir          #+#    #+#             */
-/*   Updated: 2023/11/06 13:26:31 by vdecleir         ###   ########.fr       */
+/*   Updated: 2023/11/07 10:39:01 by vdecleir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*array;
 	size_t	bytes;
-	
+
 	bytes = nmemb * size;
-	if (nmemb != 0 && size != 0)
-	{
-		if (bytes / nmemb != size || bytes % nmemb != 0)
-			return (NULL);
-	} 
 	array = malloc(bytes);
 	if (array == NULL)
 		return (NULL);
@@ -30,8 +25,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	return (array);
 }
 
+// #include <stdio.h>
 // int	main(void)
 // {
-// 	ft_calloc(9999, 9999999999);
-// 	//calloc(9999, 9999999999);
+// 	printf("%lu\n", sizeof(size_t));
+// 	printf("%p\n", ft_calloc(0, 0));
+// 	printf("%p", calloc(0, 0));
 // }
